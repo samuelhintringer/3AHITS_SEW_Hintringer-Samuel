8a35fab8db2e152086da882f4a69b1422062ddd3
samuelhintringer <samuel.hintringer@htl-braunau.at>
2026-09-17 14:13:06 +0000

    update

--- berichte/crtgitlinks.sh
+++ berichte/crtgitlinks.sh
@@ -1,32 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+reponame="$(git config --get remote.origin.url | rev | cut -d/ -f1 | rev | cut -d. -f1)"
+link="https://samuelhintringer.github.io/$reponame/berichte/"
 
 {
-    echo -e "# Arbeitsberichte" "\n"
-    for file in 2*.md; do
-        basename=$(basename "$file" .md)
-
-        jahr="20${basename:0:2}"
-        monat="${basename:2:2}"
-        tag="${basename:4:2}"
-
-        echo "[Arbeitsbericht vom ${tag}.${monat}.${jahr}](berichte/${basename}.html)"
-        echo -e "\n"
-    done
-} > ReadMeLinks.tx
-
-{
-    echo -e "# Arbeitsberichte" "\n"
-    for file in 2*.md; do
-        basename=$(basename "$file" .md)
-
-        jahr="20${basename:0:2}"
-        monat="${basename:2:2}"
-        tag="${basename:4:2}"
-
-        echo "[Arbeitsbericht vom ${tag}.${monat}.${jahr}](berichte/${basename}.html)"
-        echo -e "\n"
-    done
-} > ../README.md
+for b in $(ls 2*.md)
+do
+    echo -e $link${b/md/html}"\n"
+done
+} > gitlinks.md
 
 sleep 5
 
--- berichte/fillReadMe.sh
+++ berichte/fillReadMe.sh
@@ -1,14 +1,32 @@
-#!/usr/bin/env bash
-
-reponame="$(git config --get remote.origin.url | rev | cut -d/ -f1 | rev | cut -d. -f1)"
-link="https://samuelhintringer.github.io/$reponame/berichte/"
+#!/bin/bash
 
 {
-for b in $(ls 2*.md)
-do
-	echo -e $link${b/md/html}"\n"
-done
-} > gitlinks.md
+    echo -e "# Arbeitsberichte" "\n"
+    for file in 2*.md; do
+        basename=$(basename "$file" .md)
+
+        jahr="20${basename:0:2}"
+        monat="${basename:2:2}"
+        tag="${basename:4:2}"
+
+        echo "[Arbeitsbericht vom ${tag}.${monat}.${jahr}](berichte/${basename}.html)"
+        echo -e "\n"
+    done
+} > ReadMeLinks.tx
+
+{
+    echo -e "# Arbeitsberichte" "\n"
+    for file in 2*.md; do
+        basename=$(basename "$file" .md)
+
+        jahr="20${basename:0:2}"
+        monat="${basename:2:2}"
+        tag="${basename:4:2}"
+
+        echo "[Arbeitsbericht vom ${tag}.${monat}.${jahr}](berichte/${basename}.html)"
+        echo -e "\n"
+    done
+} > ../README.md
 
 sleep 5
 
