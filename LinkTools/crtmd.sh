4cd38e1eb7637a764a6e995de14eb2b0534c93cd
samuelhintringer <samuel.hintringer@htl-braunau.at>
2026-09-17 10:21:12 +0000

    update

---
+++ LinkTools/crtmd.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cat << EOF > $(date +"%y%m%d").md
+# Arbeitsbericht
+
+- Name: Samuel Hintringer
+- Fach: ITSI-Übunugen
+- Datum: $(date +"%d.%m.%Y")
+- Klasse: 2AHITS
+- Thema: $1
+- Aufgaben: $2
+- Source: $3
+EOF
+
